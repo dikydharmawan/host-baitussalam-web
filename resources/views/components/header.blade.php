33d822e943f0b9b33eb8434304e7aa3d3aeb673c
dikydharmawan <dikyaye118@gmail.com>
2026-02-11 14:18:51 +0700

    update-tampilan

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,17 +1,15 @@
 <div class="containerHeader">
     <div class="searchBarHearder bg-success-subtle">
-        <div class="headerchild container-fluid d-flex flex-row align-items-center">
+        <div class="headerchild container-fluid d-flex flex-row align-items-center py-2 px-lg-5">
             
             {{-- Hamburger menu for mobile (Left) --}}
             <div class="d-lg-none me-2">
                 <x-navbar></x-navbar>
             </div>
 
-            <div class="Logo py-2">
+            <div class="Logo">
                 <a class="navbar-brand m-0" href="/">
-<img src="{{ asset('assets/images/logobaitussalam.png') }}"
-     class="logo-img"
-     style="width: 85px !important; max-width: 85px !important; height: auto !important;" alt="Logo">
+                    <img src="{{ asset('assets/images/logobaitussalam.png') }}" class="logo-img" alt="Logo">
                 </a>
             </div>
 
@@ -24,7 +22,7 @@
                 @auth
                     {{-- Kalau sudah login --}}
                     <div class="dropdown">
-                        <button class="btn btn-signin d-flex align-items-center gap-2 dropdown-toggle py-2 px-3" type="button"
+                        <button class="btn btn-signin d-flex align-items-center gap-2 dropdown-toggle" type="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-person-circle fs-5"></i>
                             <span class="fw-semibold d-none d-sm-inline">
@@ -45,12 +43,12 @@
                     </div>
                 @else
                     {{-- Kalau belum login --}}
-                    <button class="btn btn-signin d-flex align-items-center gap-2 py-2 px-3" data-bs-toggle="modal"
+                    <button class="btn btn-signin d-flex align-items-center" data-bs-toggle="modal"
                         data-bs-target="#signin">
-                        <span class="fw-bold small text-uppercase">Sign In</span>
-                        <span class="icon-circle bg-white text-success d-flex align-items-center justify-content-center" style="width: 24px; height: 24px; border-radius: 50%;">
-                            <i class="bi bi-arrow-right fs-6"></i>
-                        </span>
+                        <span class="fw-bold small text-uppercase" style="font-size: 12px;">SIGN IN</span>
+                        <div class="icon-circle">
+                            <i class="bi bi-arrow-right"></i>
+                        </div>
                     </button>
                 @endauth
             </div>
