6c5e8aba20746e9994ec7ac247b663e81aff29b7
dikydharmawan <dikyaye118@gmail.com>
2026-02-11 13:02:23 +0700

    fix: robust logo sizing and tidy cards for mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,32 +1,27 @@
 <div class="containerHeader">
     <div class="searchBarHearder bg-success-subtle">
-        <div class="headerchild container-fluid d-flex flex-row align-items-center">
-
-            {{-- Hamburger menu for mobile (Left) --}}
-            <div class="d-lg-none me-2">
-                <x-navbar></x-navbar>
+        <div class="headerchild container-fluid d-flex flex-row align-items-center ">
+            <div class="navbarTop">
+                <x-Navbar></x-Navbar>
             </div>
-
-            <div class="Logo" style="max-width: 200px;">
+            <div class="Logo m-3">
                 <a class="navbar-brand" href="/">
-                    <img src="{{ asset('assets/images/logobaitussalam.png') }}" class="logo-img" alt="BaitussalamLogo"
-                        style="max-width: 100%; height: auto;">
+                    <img src="{{ asset('assets/images/logobaitussalam.png') }}" alt="BaitussalamLogo" width="120" height="96">
                 </a>
             </div>
 
-            {{-- Desktop Navbar (Center) --}}
-            <div class="d-none d-lg-flex grow justify-content-center">
-                <x-navbar></x-navbar>
-            </div>
+            <div class="ms-auto">
+                <div id="signInContainer">
 
-            <div id="signInContainer" class="ms-auto d-flex align-items-center">
-                @auth
+                    @auth
                     {{-- Kalau sudah login --}}
                     <div class="dropdown">
-                        <button class="btn btn-signin d-flex align-items-center gap-2 dropdown-toggle" type="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <button class="btn btn-signin d-flex align-items-center gap-2 dropdown-toggle"
+                            type="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
                             <i class="bi bi-person-circle"></i>
-                            <span class="fw-semibold d-none d-sm-inline">
+                            <span class="fw-semibold">
                                 {{ Auth::user()->name }}
                             </span>
                         </button>
@@ -42,17 +37,31 @@
                             </li>
                         </ul>
                     </div>
-                @else
+
+                    @else
                     {{-- Kalau belum login --}}
-                    <button class="btn btn-signin d-flex align-items-center gap-2" data-bs-toggle="modal"
+                    <button class="btn btn-signin d-flex align-items-center gap-2"
+                        data-bs-toggle="modal"
                         data-bs-target="#signin">
                         <span class="fw-semibold">SIGN IN</span>
                         <span class="icon-circle">
                             <i class="bi bi-arrow-right"></i>
                         </span>
                     </button>
-                @endauth
+                    @endauth
+
+                </div>
             </div>
-        </div>
+            {{-- <div class="fotoProfileContainer ms-auto">
+                <a href="">
+                    <img class="rounded-circle m-3" src="{{ asset('assets/images/fotoProfile.jpg') }}" alt="fotoProfile" width="50"
+            height="50">
+            </a>
+        </div> --}}
     </div>
+</div>
+<div class="navbar1">
+    <x-Navbar></x-Navbar>
+</div>
+
 </div>
