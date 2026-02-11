ed62ce8690741069883f41b9446e7cc72fc13dfd
dikydharmawan <dikyaye118@gmail.com>
2026-02-11 12:27:40 +0700

    fix: consolidate mobile CSS and refine header responsiveness

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,52 +1,57 @@
 <div class="containerHeader">
     <div class="searchBarHearder bg-success-subtle">
-        <div class="headerchild container-fluid d-flex flex-row align-items-center ">
-            <div class="Logo m-3">
+        <div class="headerchild container-fluid d-flex flex-row align-items-center justify-content-between">
+            <div class="Logo">
                 <a class="navbar-brand" href="/">
                     <img src="{{ asset('assets/images/logobaitussalam.png') }}" alt="BaitussalamLogo" width="120"
                         height="96">
                 </a>
             </div>
 
-            <div class="flex-grow-1 d-flex justify-content-center">
-                <x-navbar></x-navbar>
-            </div>
+            <div class="d-flex align-items-center gap-2 gap-lg-4">
+                <div class="d-none d-lg-block">
+                    <x-navbar></x-navbar>
+                </div>
 
-            <div id="signInContainer">
+                <div id="signInContainer" class="d-flex align-items-center gap-2">
+                    @auth
+                        {{-- Kalau sudah login --}}
+                        <div class="dropdown">
+                            <button class="btn btn-signin d-flex align-items-center gap-2 dropdown-toggle" type="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-person-circle"></i>
+                                <span class="fw-semibold d-none d-sm-inline">
+                                    {{ Auth::user()->name }}
+                                </span>
+                            </button>
 
-                @auth
-                    {{-- Kalau sudah login --}}
-                    <div class="dropdown">
-                        <button class="btn btn-signin d-flex align-items-center gap-2 dropdown-toggle" type="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-person-circle"></i>
-                            <span class="fw-semibold">
-                                {{ Auth::user()->name }}
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item text-danger">
+                                            <i class="bi bi-box-arrow-right me-2"></i> Logout
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+                    @else
+                        {{-- Kalau belum login --}}
+                        <button class="btn btn-signin d-flex align-items-center gap-2" data-bs-toggle="modal"
+                            data-bs-target="#signin">
+                            <span class="fw-semibold">SIGN IN</span>
+                            <span class="icon-circle">
+                                <i class="bi bi-arrow-right"></i>
                             </span>
                         </button>
+                    @endauth
 
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li>
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item text-danger">
-                                        <i class="bi bi-box-arrow-right me-2"></i> Logout
-                                    </button>
-                                </form>
-                            </li>
-                        </ul>
+                    {{-- Hamburger menu for mobile --}}
+                    <div class="d-lg-none">
+                        <x-navbar></x-navbar>
                     </div>
-                @else
-                    {{-- Kalau belum login --}}
-                    <button class="btn btn-signin d-flex align-items-center gap-2" data-bs-toggle="modal"
-                        data-bs-target="#signin">
-                        <span class="fw-semibold">SIGN IN</span>
-                        <span class="icon-circle">
-                            <i class="bi bi-arrow-right"></i>
-                        </span>
-                    </button>
-                @endauth
-
+                </div>
             </div>
         </div>
     </div>
